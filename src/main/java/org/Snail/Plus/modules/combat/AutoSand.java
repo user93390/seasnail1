9b1a5f02697dbc1145cddcab21dd36393e851a29
Seasnail1 <samueltwillows@hotmail.com>
2024-07-08 15:56:28 -0700

    update AutoSand.java
    fixed and rewrote the whole module 😀

--- src/main/java/org/Snail/Plus/modules/combat/AutoSand.java
+++ src/main/java/org/Snail/Plus/modules/combat/AutoSand.java
@@ -9,15 +9,15 @@ import meteordevelopment.meteorclient.utils.entity.SortPriority;
 import meteordevelopment.meteorclient.utils.entity.TargetUtils;
 import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
-import meteordevelopment.meteorclient.utils.render.RenderUtils;
 import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import meteordevelopment.meteorclient.utils.world.BlockUtils;
+import meteordevelopment.meteorclient.utils.world.CardinalDirection;
 import meteordevelopment.orbit.EventHandler;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
 import org.Snail.Plus.Addon;
+import org.Snail.Plus.utils.CombatUtils;
 
 import java.util.Objects;
 
@@ -51,16 +51,6 @@ public class AutoSand extends Module {
             .description("How to filter targets within range.")
             .defaultValue(SortPriority.LowestDistance)
             .build());
-
-    private final Setting<Boolean> support = sgGeneral.add(new BoolSetting.Builder()
-            .name("support")
-            .description("Support (Obsidian)")
-            .defaultValue(false)
-            .build());
-
-    private static Setting<SettingColor> SideColor;
-    private static Setting<SettingColor> LineColor;
-
     private final Setting<Double> delay = sgGeneral.add(new DoubleSetting.Builder()
             .name("delay")
             .description("Delay in seconds between each block placement.")
@@ -69,45 +59,65 @@ public class AutoSand extends Module {
             .sliderMax(5)
             .build());
 
+    private final Setting<Boolean> strictDirection = sgGeneral.add(new BoolSetting.Builder()
+            .name("strict direction")
+            .description("uses strict direction")
+            .defaultValue(false)
+            .build());
+
+    private final Setting<Boolean> support = sgGeneral.add(new BoolSetting.Builder()
+            .name("support")
+            .description("Support (Obsidian)")
+            .defaultValue(false)
+            .build());
+
     private final Setting<Boolean> autoDisable = sgGeneral.add(new BoolSetting.Builder()
             .name("auto-disable")
             .description("Disables the module when you have placed the sand")
             .defaultValue(true)
             .build());
 
-    private long lastPlaceTime = 0;
+    private final Setting<Boolean> onlySurrounded = sgGeneral.add(new BoolSetting.Builder()
+            .name("only surrounded")
+            .description("only targets players if they are surrounded")
+            .defaultValue(true)
+            .build());
+    private final Setting<SettingColor> sideColor = sgGeneral.add(new ColorSetting.Builder()
+            .name("side color")
+            .description("Side color")
+            .defaultValue(new SettingColor(255, 0, 0, 75))
+            .build());
+    private final Setting<SettingColor> lineColor = sgGeneral.add(new ColorSetting.Builder()
+            .name("line color")
+            .description("Line color")
+            .defaultValue(new SettingColor(255, 0, 0, 255))
+            .build());
 
+
+    private long lastPlaceTime = 0;
+    private boolean sandPlaced;
     public AutoSand() {
         super(Addon.Snail, "auto-sand", "Places sand two blocks above players' heads");
-
-        // Initialize static variables
-        SideColor = sgGeneral.add(new ColorSetting.Builder()
-                .name("Side Color")
-                .description("side color")
-                .defaultValue(new SettingColor(255, 0, 0, 80))
-                .build());
-
-        LineColor = sgGeneral.add(new ColorSetting.Builder()
-                .name("Line color")
-                .description("line color")
-                .defaultValue(new SettingColor(255, 0, 0, 255))
-                .build());
     }
     @EventHandler
     private void onTick(TickEvent.Pre event) {
         long time = System.currentTimeMillis();
-
         if ((time - lastPlaceTime) < delay.get() * 1000) return;
-
         lastPlaceTime = time;
+
         PlayerEntity target = TargetUtils.getPlayerTarget(range.get(), priority.get());
         if(TargetUtils.isBadTarget(target, range.get())) return;
 
-        FindItemResult Sand = InvUtils.findInHotbar(Items.SAND, Items.RED_SAND, Items.GRAVEL);
         FindItemResult Support = InvUtils.findInHotbar(Items.OBSIDIAN);
         BlockPos targetPos = target.getBlockPos().up(height.get());
 
-        if (Objects.requireNonNull(mc.world).getBlockState(targetPos).getBlock().equals(Blocks.AIR) && Sand != null && target != null) {
+        if (onlySurrounded.get() && CombatUtils.isSurrounded(target) && Objects.requireNonNull(mc.world).getBlockState(targetPos).isAir()) {
+
+            for (CardinalDirection dir : CardinalDirection.values()) {
+                if (this.strictDirection.get()
+                        && dir.toDirection() != Objects.requireNonNull(mc.player).getHorizontalFacing()
+                        && dir.toDirection().getOpposite() != mc.player.getHorizontalFacing()) continue;
+            }
 
             if (this.support.get()) {
                 BlockPos supportPosNorth = target.getBlockPos().north(1);
@@ -118,12 +128,8 @@ public class AutoSand extends Module {
                 BlockUtils.place(supportPosNorthUpOne, Support, rotate.get(), 0, true);
                 BlockUtils.place(supportPosNorthUpTwo, Support, rotate.get(), 0, true);
             }
-
             BlockUtils.place(targetPos, InvUtils.findInHotbar(Items.SAND, Items.RED_SAND, Items.GRAVEL), rotate.get(), 0, false);
-
-            boolean sandPlaced = BlockUtils.place(targetPos, InvUtils.findInHotbar(Items.ANVIL), rotate.get(), 0, false);
-            RenderUtils.renderTickingBlock(targetPos, SideColor.get(), LineColor.get(), ShapeMode.Both, 5, 5, true, false);
-
+            sandPlaced = true;
             if (this.autoDisable.get() && sandPlaced) {
                 this.toggle();
             }
@@ -133,17 +139,20 @@ public class AutoSand extends Module {
     @EventHandler
     private void onRender(Render3DEvent event) {
         PlayerEntity target = TargetUtils.getPlayerTarget(range.get(), priority.get());
-        if (target == null) return;
-
-        BlockPos targetPos = target.getBlockPos().up(height.get());
-        event.renderer.box(targetPos, SideColor.get(), LineColor.get(), ShapeMode.Both, (int) 1.0f);
-
-        BlockPos supportPosNorth = target.getBlockPos().north(1);
+        if(onlySurrounded.get() && !CombatUtils.isSurrounded(target)) return;
+        BlockPos supportPosNorth = Objects.requireNonNull(target).getBlockPos().north(1);
         BlockPos supportPosNorthUpOne = target.getBlockPos().north(1).up(1);
         BlockPos supportPosNorthUpTwo = target.getBlockPos().north(1).up(2);
 
-        event.renderer.box(supportPosNorth, SideColor.get(), LineColor.get(), ShapeMode.Both, 5);
-        event.renderer.box(supportPosNorthUpOne, SideColor.get(), LineColor.get(), ShapeMode.Both, 5);
-        event.renderer.box(supportPosNorthUpTwo, SideColor.get(), LineColor.get(), ShapeMode.Both, 5);
+
+        BlockPos targetPos = target.getBlockPos().up(height.get());
+        event.renderer.box(targetPos, sideColor.get(), lineColor.get(), ShapeMode.Both, (int) 1.0f);
+
+
+        if (support.get()) {
+            event.renderer.box(supportPosNorth, sideColor.get(), lineColor.get(), ShapeMode.Both, 5);
+            event.renderer.box(supportPosNorthUpOne, sideColor.get(), lineColor.get(), ShapeMode.Both, 5);
+            event.renderer.box(supportPosNorthUpTwo, sideColor.get(), lineColor.get(), ShapeMode.Both, 5);
+        }
     }
 }
