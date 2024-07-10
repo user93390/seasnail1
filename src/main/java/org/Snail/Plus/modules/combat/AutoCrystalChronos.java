0d6ea07708e3fb62ff0483544327316dae2c8b88
chronos <vjavle1@outlook.com>
2024-07-09 20:28:24 -0500

    update 2.3

--- src/main/java/org/Snail/Plus/modules/combat/AutoCrystalChronos.java
+++ src/main/java/org/Snail/Plus/modules/combat/AutoCrystalChronos.java
@@ -6,6 +6,8 @@ import meteordevelopment.meteorclient.renderer.ShapeMode;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.combat.Offhand;
+import meteordevelopment.meteorclient.utils.entity.DamageUtils;
+import meteordevelopment.meteorclient.utils.entity.EntityUtils;
 import meteordevelopment.meteorclient.utils.entity.SortPriority;
 import meteordevelopment.meteorclient.utils.entity.TargetUtils;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
@@ -14,9 +16,14 @@ import meteordevelopment.meteorclient.utils.render.color.Color;
 import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import meteordevelopment.meteorclient.utils.world.BlockUtils;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.decoration.EndCrystalEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.RotationCalculator;
@@ -48,6 +55,11 @@ public class AutoCrystalChronos extends Module {
             .description("Automatically rotates towards the position where the block is being broken.")
             .defaultValue(true)
             .build());
+    private final Setting<Boolean> antisuicide = sgGeneral.add(new BoolSetting.Builder()
+            .name("antisuicide")
+            .description("Antisuicide")
+            .defaultValue(false)
+            .build());
 
     private final Setting<Double> delay = sgGeneral.add(new DoubleSetting.Builder()
             .name("delay")
@@ -56,7 +68,6 @@ public class AutoCrystalChronos extends Module {
             .min(0)
             .sliderMax(5)
             .build());
-
 
 
     public AutoCrystalChronos() {
@@ -71,22 +82,41 @@ public class AutoCrystalChronos extends Module {
 
         BlockPos targetPos = target.getBlockPos();
 
+
         long time = System.currentTimeMillis();
         if ((time - lastPlaceTime) < delay.get() * 1000) return;
         lastPlaceTime = time;
 
-        if (CombatUtils.isSurrounded(target) == false & BlockUtils.canPlace(targetPos.north(1)))
-        {
-            BlockUtils.place(targetPos.north(1),InvUtils.findInHotbar(Items.END_CRYSTAL),rotate.get(),0,false);
-            BlockUtils.breakBlock(targetPos.north(1), true);
-            Objects.requireNonNull(mc.getNetworkHandler()).sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.START_DESTROY_BLOCK, targetPos.north(2), Direction.DOWN));
-            mc.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.STOP_DESTROY_BLOCK, targetPos.north(1), Direction.DOWN));
-            RenderUtils.renderTickingBlock(targetPos.north(1).down(1), Color.RED, Color.RED, ShapeMode.Both, 0, 10, true, false);
+
+            if (CombatUtils.isSurrounded(target) == false & BlockUtils.canPlace(targetPos.north(1)) & !BlockUtils.canPlace(targetPos.north(1).down(1))) {
+                BlockUtils.place(targetPos.north(1), InvUtils.findInHotbar(Items.END_CRYSTAL), rotate.get(), 0, false);
+
+                RenderUtils.renderTickingBlock(targetPos.north(1).down(1), Color.RED, Color.RED, ShapeMode.Both, 0, 10, true, false);
+
+            }
+
+            else if (CombatUtils.isSurrounded(target) == false & BlockUtils.canPlace(targetPos.east(1))& !BlockUtils.canPlace(targetPos.east(1).down(1))) {
+            BlockUtils.place(targetPos.east(1), InvUtils.findInHotbar(Items.END_CRYSTAL), rotate.get(), 0, false);
+
+            RenderUtils.renderTickingBlock(targetPos.east(1).down(1), Color.RED, Color.RED, ShapeMode.Both, 0, 10, true, false);
+            }
+
+
+            else if (CombatUtils.isSurrounded(target) == false & BlockUtils.canPlace(targetPos.south(1))& !BlockUtils.canPlace(targetPos.south(1).down(1))) {
+            BlockUtils.place(targetPos.south(1), InvUtils.findInHotbar(Items.END_CRYSTAL), rotate.get(), 0, false);
+
+            RenderUtils.renderTickingBlock(targetPos.south(1).down(1), Color.RED, Color.RED, ShapeMode.Both, 0, 10, true, false);
+            }
+
+
+            else if (CombatUtils.isSurrounded(target) == false & BlockUtils.canPlace(targetPos.west(1))& !BlockUtils.canPlace(targetPos.west(1).down(1))) {
+            BlockUtils.place(targetPos.west(1), InvUtils.findInHotbar(Items.END_CRYSTAL), rotate.get(), 0, false);
+
+            RenderUtils.renderTickingBlock(targetPos.west(1).down(1), Color.RED, Color.RED, ShapeMode.Both, 0, 10, true, false);
+            }
+
+
+
         }
-    }
-
-    @EventHandler
-    private  void render(Renderer3D event) {
 
     }
-}
