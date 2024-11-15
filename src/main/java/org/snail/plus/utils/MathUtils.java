f01a0f3190f5c75c735995fb892bdde9db9818ba
Samuel Willows <samueltwillows@hotmail.com>
2024-11-14 20:17:44 -0800

    Update MathUtils.java

--- src/main/java/org/snail/plus/utils/MathUtils.java
+++ src/main/java/org/snail/plus/utils/MathUtils.java
@@ -1,16 +1,13 @@
 package org.snail.plus.utils;
 
-
-import meteordevelopment.meteorclient.utils.player.Rotations;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.RaycastContext;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import static meteordevelopment.meteorclient.MeteorClient.mc;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 
 public class MathUtils {
 
@@ -44,23 +41,9 @@ public class MathUtils {
         return Math.sqrt(zx * zx + y * y);
     }
 
-    public static void rayCast(BlockPos blockPos, int rotationSteps) {
-        Vec3d playerPos = mc.player.getPos().add(0, mc.player.getEyeHeight(mc.player.getPose()), 0);
-        Vec3d blockTopCenter = new Vec3d(blockPos.getX() + 0.5, blockPos.getY() + 1, blockPos.getZ() + 0.5);
-
-        double diffX = blockTopCenter.x - playerPos.x;
-        double diffY = blockTopCenter.y - playerPos.y;
-        double diffZ = blockTopCenter.z - playerPos.z;
-
-        double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
-
-        float initialYaw = (float) Math.toDegrees(Math.atan2(diffZ, diffX)) - 90;
-        float pitch = (float) -Math.toDegrees(Math.atan2(diffY, diffXZ));
-
-
-        if (mc.world.raycast(new RaycastContext(playerPos, blockTopCenter, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, mc.player)).getType() != HitResult.Type.MISS) {
-            Rotations.rotate(initialYaw, pitch);
-        }
+    public static boolean rayCast(Vec3d blockPos) {
+        HitResult hitResult = mc.world.raycast(new RaycastContext(new Vec3d(mc.player.getX(), mc.player.getEyeY(), mc.player.getZ()), blockPos, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, mc.player));
+        return hitResult.getType() != HitResult.Type.MISS;
     }
 
     public static void updateRotation(int steps) {
@@ -70,6 +53,7 @@ public class MathUtils {
             mc.player.setYaw(interpolatedYaw);
             mc.player.setPitch(interpolatedPitch);
             currentStep++;
+
         }
     }
 }
