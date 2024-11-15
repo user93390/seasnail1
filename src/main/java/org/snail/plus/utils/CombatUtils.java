80a1b5344ef6b4d6d1cdfed2ec50fa2358f726e2
Samuel Willows <samueltwillows@hotmail.com>
2024-11-14 20:16:33 -0800

    Update CombatUtils.java

--- src/main/java/org/snail/plus/utils/CombatUtils.java
+++ src/main/java/org/snail/plus/utils/CombatUtils.java
@@ -1,102 +1,67 @@
 package org.snail.plus.utils;
 
+import java.util.List;
+
+import static meteordevelopment.meteorclient.MeteorClient.mc;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
-import java.util.List;
-import java.util.Objects;
-
-import static meteordevelopment.meteorclient.MeteorClient.mc;
-
 public class CombatUtils {
 
-    private static boolean IsValidBlock(BlockPos pos) {
-        Block block = Objects.requireNonNull(mc.world).getBlockState(pos).getBlock();
-        return block == Blocks.OBSIDIAN || block == Blocks.BEDROCK || block == Blocks.REINFORCED_DEEPSLATE || block == Blocks.NETHERITE_BLOCK || block == Blocks.CRYING_OBSIDIAN || block == Blocks.ENDER_CHEST || block == Blocks.ANVIL;
+    private static boolean isValidBlock(BlockPos pos) {
+        Block block = mc.world.getBlockState(pos).getBlock();
+        return block.equals(Blocks.OBSIDIAN) || block.equals(Blocks.BEDROCK) || block.equals(Blocks.REINFORCED_DEEPSLATE) || block.equals(Blocks.NETHERITE_BLOCK) || block.equals(Blocks.CRYING_OBSIDIAN) || block.equals(Blocks.ENDER_CHEST) || block.equals(Blocks.ANVIL);
     }
 
     public static boolean isCentered(PlayerEntity target) {
         BlockPos blockPos = target.getBlockPos();
         double centerX = blockPos.getX() + 0.5;
         double centerZ = blockPos.getZ() + 0.5;
-        double playerX = target.getX();
-        double playerZ = target.getZ();
-        double distanceX = Math.abs(playerX - centerX);
-        double distanceZ = Math.abs(playerZ - centerZ);
         double threshold = 0.2;
-        return distanceX < threshold && distanceZ < threshold;
+        return Math.abs(target.getX() - centerX) < threshold && Math.abs(target.getZ() - centerZ) < threshold;
     }
-
 
     public static boolean isBurrowed(PlayerEntity target) {
         Vec3d pos = new Vec3d(target.getX(), target.getY() + 0.4, target.getZ());
-        return target.getY() == target.prevY && IsValidBlock(BlockPos.ofFloored(pos));
+        return target.getY() == target.prevY && isValidBlock(BlockPos.ofFloored(pos));
     }
 
-    public static PlayerEntity filter(List<AbstractClientPlayerEntity> playerEntities, filterMode Mode) {
-        PlayerEntity target = null;
-        double distance = 0;
-        double health = 0;
-
+    public static PlayerEntity filter(List<AbstractClientPlayerEntity> playerEntities, filterMode mode, double range) {
         playerEntities.remove(mc.player);
         playerEntities.removeAll(WorldUtils.getAllFriends());
+        return playerEntities.stream()
+                .filter(player -> mc.player != null && mc.player.distanceTo(player) <= range)
+                .min((player1, player2) -> {
+                    double distance1 = mc.player.distanceTo(player1);
+                    double distance2 = mc.player.distanceTo(player2);
+                    double health1 = player1.getHealth();
+                    double health2 = player2.getHealth();
 
-
-        for (PlayerEntity player : playerEntities) {
-            double currentDistance = mc.player.distanceTo(player);
-            double currentHealth = player.getHealth();
-
-            switch (Mode) {
-                case Closet -> {
-                    if (distance == 0 || currentDistance < distance) {
-                        distance = currentDistance;
-                        target = player;
-                    }
-                }
-                case Furthest -> {
-                    if (distance == 0 || currentDistance > distance) {
-                        distance = currentDistance;
-                        target = player;
-                    }
-                }
-                case LowestHealth -> {
-                    if (health == 0 || currentHealth < health) {
-                        health = currentHealth;
-                        target = player;
-                    }
-                }
-                case HighestHealth -> {
-                    if (health == 0 || currentHealth > health) {
-                        health = currentHealth;
-                        target = player;
-                    }
-                }
-            }
-        }
-        return target;
+                    return switch (mode) {
+                        case Closet -> Double.compare(distance1, distance2);
+                        case Furthest -> Double.compare(distance2, distance1);
+                        case LowestHealth -> Double.compare(health1, health2);
+                        case HighestHealth -> Double.compare(health2, health1);
+                    };
+                }).orElse(null);
     }
 
-    public static boolean isSurrounded(PlayerEntity player) {
-        BlockPos pos = player.getBlockPos();
-        BlockPos[] blocks = {
-                pos.north(), pos.south(), pos.east(), pos.west()
-        };
-
-        for (BlockPos block : blocks) {
-            if (WorldUtils.isAir(block)) {
-                return false;
-            }
+    public static PlayerEntity getLastAttacker(Entity entity) {
+        if (!entity.isAlive() && entity instanceof LivingEntity livingEntity) {
+            return (PlayerEntity) livingEntity.getAttacker();
         }
-        return true;
+        return null;
     }
 
-    public enum DmgTye {
-        Bed,
-        crystal,
+    public static boolean isSurrounded(PlayerEntity target) {
+        BlockPos blockPos = target.getBlockPos();
+        return isValidBlock(blockPos.north()) && isValidBlock(blockPos.south()) && isValidBlock(blockPos.east()) && isValidBlock(blockPos.west());
     }
 
     public enum filterMode {
