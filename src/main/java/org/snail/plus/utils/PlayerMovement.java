254d581dfc0cc2a0d99af1ac7693de79e39629a4
Samuel Willows <samueltwillows@hotmail.com>
2024-11-19 13:51:38 -0800

    whoops

---
+++ src/main/java/org/snail/plus/utils/PlayerMovement.java
@@ -0,0 +1,17 @@
+package org.snail.plus.utils;
+
+public class PlayerMovement {
+    public double x;
+    public double y;
+    public double z;
+    public float yaw;
+    public float pitch;
+
+    public PlayerMovement(double x, double y, double z, float yaw, float pitch) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.yaw = yaw;
+        this.pitch = pitch;
+    }
+}
